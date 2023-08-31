18b9d1fbeef735e8f8ca19d0472c654f690dc8c8
LakshayBhandari <LakshayBhandari867@gmail.com>
2023-08-31 15:25:37 +0530

    added delete functionality

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useBoardStore } from '@/store/BoardStore';
 import { Todo, TypedColumn } from '@/typings'
 import { XCircleIcon } from '@heroicons/react/20/solid';
 import React from 'react'
@@ -21,6 +22,8 @@ function TodoCard({
     draggableProps,
     dragHandleProps
 }:Props) {
+
+  const deleteTask= useBoardStore((state) => state.deleteTask)
   return (
     <div
     className='bg-white rounded-md space-y-2 drop-shadow-md'
@@ -30,7 +33,7 @@ function TodoCard({
     >
       <div className='flex items-center justify-between p-5'>
           <p>{todo.title}</p>
-          <button className='text-red-500 hover:text-red-600'>
+          <button onClick={() => deleteTask(index,todo,id)} className='text-red-500 hover:text-red-600'>
             <XCircleIcon
             className='ml-5 h-8 w-8'
             >
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,4 +1,4 @@
-import { databases } from "@/appwrite";
+import { databases, storage } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/GetTodosGroupedByColumn";
 import { Board, Column, Todo, TypedColumn } from "@/typings";
 import { Permission, Role } from "appwrite";
@@ -13,8 +13,10 @@ interface BoardState {
 
     searchString:string;
     setSearchString : (searchString:string) => void;
+
+    deleteTask:(taskIndex:number,todoId:Todo,id:TypedColumn) => void;
 }
-export const useBoardStore=create<BoardState>((set)=>({
+export const useBoardStore=create<BoardState>((set,get)=>({
     board:{
         columns: new Map<TypedColumn,Column>()
     },
@@ -37,6 +39,25 @@ export const useBoardStore=create<BoardState>((set)=>({
             title:todo.title,
             status:columnId,
         },
+      )
+    },
+    deleteTask: async (taskIndex:number,todo:Todo,id:TypedColumn)=>{
+     
+      const newColumns = new Map(get().board.columns);
+
+      //delete todoId from newColums
+
+      newColumns.get(id)?.todos.splice(taskIndex,1)
+      set({ board :{columns : newColumns}});
+
+      if(todo.image){
+        await storage.deleteFile(todo.image.bucketId, todo.image.fileId)
+      }
+
+      await databases.deleteDocument(
+        process.env.NEXT_PUBLIC_DATABASE_ID!,
+        process.env.NEXT_PUBLIC_COLLECTION_ID!,
+        todo.$id
       )
     }
 }))
